b302ce569ac7f98a97947ce4e66f63bae85a0c66
Caleb Brown <calebgcbrown@gmail.com>
2021-04-24 13:23:03 +0100

    Colors added

---
+++ colors.js
@@ -0,0 +1,31 @@
+export const themeBlack = "#1E1E1E";
+export const themeLightGray = "#F2F2F2";
+
+export const themeLightBlue = "#98FCFF";
+export const themeLightCyan = "#81CDCF";
+export const themeCyan = "#04B7BC";
+export const themeDarkCyan = "#219497";
+export const themeDarkPurple = "#4C1F7D";
+export const themePurple = "#5100B1";
+export const themeHotPurple = "#7217F4";
+export const themeRed = "#F741F0";
+export const themeYellow = "#fffa65";
+export const themeDarkYellow = "#E5DF40";
+export const themeHotPink = "#FA34F3";
+export const themeDarkPink = "#CF15F9";
+export const themeHotTeal = "#37F4F1";
+
+export const buttonActive = themeBlack;
+export const linkColor = "#b6509e";
+export const buttonHover = "";
+export const buttonInactive = "#d1d8e0";
+
+export const themeDarkGreen = "#219497";
+
+export const commonColor = "#8064FF";
+export const uncommonColor = "#48ABFF";
+export const rareColor = "#FF96FF";
+export const legendaryColor = "#51FFA8";
+
+export const twitterLogoBlue = "#00acee";
+export const twitterDarkBlue = "#0084b4";
